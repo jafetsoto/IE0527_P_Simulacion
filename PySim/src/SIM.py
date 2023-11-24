ef6babe0bd300272a856612c1f4f0592a94fba97
Soto <jafets.a22@gmail.com>
2023-11-23 23:26:28 -0600

    [ACT] Llamado a Hamming en archivo de simulación principal, he impresión correcta de los datos.

--- PySim/src/SIM.py
+++ PySim/src/SIM.py
@@ -3,26 +3,32 @@
 # Bibliotecas:
 import numpy as np
 from CODIFICADOR import *
+from HAMMING import HAMMING
 # ---------------------------------------------------------
 # =========================================================
 # ----------------------- Main: ---------------------------
-# Carga del mensaje que enviaremos:
 def GET_MENSAJE():
     '''Se carga el archivo de texto, enviado desde la casa para simular.
         Outputs: TEXT (varible que contiene el archivo de texto.)'''
     with open('PySim/datasets/INPUT.txt', 'r') as file:
         TEXT = file.read()
     return TEXT
-
-# Codificar el mensaje por el algoritmo de Huffman:
+# ---------------------------------------------------------
+# Cargae el mensaje:
 TEXT = GET_MENSAJE()
+
+# Codificar por Huffman para la funete:
 TEXT_CODIF, CODIGOS = CODIFICADOR(TEXT)
+
+# Codificar por Hamming para el canal:
+TEXT_CANAL = HAMMING(TEXT_CODIF)
 
 # ---------------------------------------------------------
 print("============================================================")
-print("Mensaje de entrada:\n", TEXT)
-print("Mensaje Codificado:\n", TEXT_CODIF)
+print("Mensaje de entrada   :\n", TEXT)
+print("Mensaje Codif Huffman:\n", TEXT_CODIF)
 print("Codigos:\n", CODIGOS)
+print("Mensaje Codif Hamming:\n", TEXT_CANAL)
 print("============================================================")
 # ---------------------------------------------------------
 # =========================================================
