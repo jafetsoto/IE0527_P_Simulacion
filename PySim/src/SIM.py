d8724d067e8ec8e38229980c423f9cc7db47da18
Soto <jafets.a22@gmail.com>
2023-11-27 23:09:51 -0600

    [ACT] Estamos trabajando con una prueba pequeña en el main para controlar el comportamiento del sistema.

--- PySim/src/SIM.py
+++ PySim/src/SIM.py
@@ -1,12 +1,13 @@
 # =========================================================
 # ---------------------------------------------------------
 # Bibliotecas:
+from tkinter.tix import TEXT
 import numpy as np
 import matplotlib.pyplot as plt
 
 from CODIFICADOR import *
 from HAMMING import HAMMING
-from PSK import PSK
+from PSK import *
 from TRANSMISION import Transmision
 # ---------------------------------------------------------
 # =========================================================
@@ -17,14 +18,14 @@ def GET_MENSAJE():
     with open('PySim/datasets/INPUT.txt', 'r') as file:
         TEXT = file.read()
     return TEXT
-def graf_Signal(PSK_SIGNAL, fs):                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
+def graf_Signal(PSK_SIGNAL, fs, TITLE):                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
     # Tiempo de la señal:
     tiempo_total = np.linspace(0, len(PSK_SIGNAL ) / fs, len(PSK_SIGNAL))
     
     # Graficar la señal:
     plt.figure(figsize=(10, 4))
     plt.plot(tiempo_total, PSK_SIGNAL)
-    plt.title('Señal.')
+    plt.title(TITLE)
     plt.xlabel('Tiempo (s)')
     plt.ylabel('Amplitud')
     plt.grid(True)
@@ -38,15 +39,18 @@ TEXT_CODIF, CODIGOS = CODIFICADOR(TEXT)
 
 # Codificar por Hamming para el canal:
 TEXT_CANAL = HAMMING(TEXT_CODIF)
-
+TEXT_PRUEBA = '110101011101'
 # Modular la señal:
-SIGNAL_PSK   = PSK(TEXT_CANAL)
-graf_Signal(SIGNAL_PSK, 1000)
+SIGNAL_PSK   = BPSK(TEXT_PRUEBA)
+graf_Signal(SIGNAL_PSK, 1000, 'Señal modulada (BPSK).')
 
 # Simular transmisión:
-SIGNAL_TRANSMIT = Transmision(SIGNAL_PSK, 1000)
-graf_Signal(SIGNAL_TRANSMIT, 1000)
+SIGNAL_TRANSMIT = Transmision(SIGNAL_PSK)
+graf_Signal(SIGNAL_TRANSMIT, 1000, 'Señal transmitida.')
 
+# Demodular la señal recibida: 
+DEMOD_TEXT = DEMOD_BPSK(SIGNAL_TRANSMIT, 1000)
+graf_Signal(DEMOD_TEXT,1000, 'Señal demodulada.')
 # ---------------------------------------------------------
 print("============================================================")
 print("Mensaje de entrada   :\n", TEXT)
